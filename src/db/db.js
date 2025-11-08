36a57b4a3066054ca68747710595ec9bc13f46c2
Arshad Faraz <arshadfarazusmani@gmail.com>
2025-11-08 19:40:38 +0530

    add db connection

---
+++ src/db/db.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+dotenv.config({
+    path:".env"
+})
+import mongoose from "mongoose"
+import { db_name } from "../constant.js"
+
+const db_uri = process.env.MONGO_URI
+// const db_uri ="mongodb+srv://arshadfarazusmani:gulraz@cluster0.nwpakgu.mongodb.net/"
+
+const connectDB= async function(){  // function definition 
+   try{
+    const connectionInstance= await mongoose.connect(`${db_uri}${db_name}`)
+    console.log("Database connected successfully to", connectionInstance.connection.name)
+   }catch(error){
+    
+    
+    console.log("DB connection Error !!!",error)
+    process.exit(1);
+
+   }
+};
+
+export{connectDB}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Hello  !!")
+import dotenv from "dotenv";
+dotenv.config({
+    path:".env"
+})
+
+import { app } from "./app.js";
+import { connectDB } from "./db/db.js";
+
+
+const PORT= process.env.PORT
+
+
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("server is running on ",PORT)
+    })
+
+}).catch((error)=>{
+    console.log("Database connection error !!!",error)
+
+})
+
+
